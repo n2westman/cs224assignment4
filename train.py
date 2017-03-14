ecd391b73efa851a0129d0b56e43dcde195ea342
Joris van Mens <jorisvanmens@gmail.com>
2017-03-13 20:29:53 -0700

    Wiring up FLAGS & Config throughout all code

--- train.py
+++ train.py
@@ -27,9 +27,13 @@ tf.app.flags.DEFINE_float("max_gradient_norm", 10.0, "Clip gradients to this nor
 tf.app.flags.DEFINE_float("dropout", 0.15, "Fraction of units randomly dropped on non-recurrent connections.")
 tf.app.flags.DEFINE_integer("batch_size", 200, "Batch size to use during training.")
 tf.app.flags.DEFINE_integer("epochs", 10, "Number of epochs to train.")
-tf.app.flags.DEFINE_integer("state_size", 200, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("state_size", 200, "Size of each model layer.") # Not used
 tf.app.flags.DEFINE_integer("output_size", 600, "The output size of your model.")
 tf.app.flags.DEFINE_integer("embedding_size", 100, "Size of the pretrained vocabulary.")
+tf.app.flags.DEFINE_integer("n_hidden_enc", 200, "Number of nodes in the LSTMs of the encoder.")
+tf.app.flags.DEFINE_integer("n_hidden_mix", 200, "Number of nodes in the LSTMs of the mixer.")
+tf.app.flags.DEFINE_integer("n_hidden_dec_v3", 20, "Number of nodes in the hidden layer of decoder V3.")
+tf.app.flags.DEFINE_integer("n_hidden_dec_hmn", 50, "Number of nodes in the hidden layer of the HMN.")
 tf.app.flags.DEFINE_integer("max_examples", sys.maxint, "Number of examples over which to iterate")
 tf.app.flags.DEFINE_string("data_dir", "data/squad", "SQuAD directory (default ./data/squad)")
 tf.app.flags.DEFINE_string("train_dir", "train", "Training directory to save the model parameters (default: ./train).")
@@ -40,7 +44,7 @@ tf.app.flags.DEFINE_integer("print_every", 1, "How many iterations to do per pri
 tf.app.flags.DEFINE_integer("keep", 0, "How many checkpoints to keep, 0 indicates keep all.")
 tf.app.flags.DEFINE_string("vocab_path", "data/squad/vocab.dat", "Path to vocab file (default: ./data/squad/vocab.dat)")
 tf.app.flags.DEFINE_string("embed_path", "", "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
-tf.app.flags.DEFINE_string("model", "baseline", "Model: baseline or MHN (default: baseline)")
+tf.app.flags.DEFINE_string("model", "baseline", "Model: baseline or MHN (default: baseline).")
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -185,31 +189,24 @@ def main(_):
     # Mix of pre-fab code and our code
     # First function that is called when running code. Loads data, defines a few things and calls train()
 
-    max_context_length = FLAGS.output_size
     dataset = {
-        'train': load_and_preprocess_dataset(FLAGS.data_dir, 'train', max_context_length, max_examples=FLAGS.max_examples),
-        'val': load_and_preprocess_dataset(FLAGS.data_dir, 'val', max_context_length, max_examples=FLAGS.max_examples)
+        'train': load_and_preprocess_dataset(FLAGS.data_dir, 'train', FLAGS.output_size, FLAGS.max_examples),
+        'val': load_and_preprocess_dataset(FLAGS.data_dir, 'val', FLAGS.output_size, FLAGS.max_examples)
     }
 
     embed_path = FLAGS.embed_path or pjoin("data", "squad", "glove.trimmed.{}.npz".format(FLAGS.embedding_size))
     vocab_path = FLAGS.vocab_path or pjoin(FLAGS.data_dir, "vocab.dat")
     vocab, rev_vocab = initialize_vocab(vocab_path)
 
-    configKwargs = {
-        'batch_size': FLAGS.batch_size,
-        'learning_rate': FLAGS.learning_rate,
-        'dropout': FLAGS.dropout,
-        'test': FLAGS.test,
-    }
-    config = Config(**configKwargs)
-    encoder = Encoder(size=FLAGS.state_size, vocab_dim=FLAGS.embedding_size)
+    config = Config(FLAGS)
+    encoder = Encoder(FLAGS)
     if FLAGS.model == 'baseline':
-        decoder = Decoder(output_size=FLAGS.output_size, batch_size=config.batch_size)
+        decoder = Decoder(FLAGS)
     else:
-        decoder = HMNDecoder(output_size=FLAGS.output_size, batch_size=config.batch_size)
-    mixer = Mixer()
+        decoder = HMNDecoder(FLAGS)
+    mixer = Mixer(FLAGS)
 
-    qa = QASystem(encoder, decoder, mixer, embed_path, max_context_length, config, FLAGS.model)
+    qa = QASystem(encoder, decoder, mixer, embed_path, config, FLAGS.model)
 
     if not os.path.exists(FLAGS.log_dir):
         os.makedirs(FLAGS.log_dir)
