61301f2504af382c2517d652dfe407fb50f7250a
Nickolas Westman <nwestman@nwestman-macbookpro.roam.corp.google.com>
2017-03-15 12:16:19 -0700

    Create data processing utils file

--- data_utils_test.py
+++ data_utils_test.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
-from train import load_and_preprocess_dataset
+from data_utils import load_and_preprocess_dataset
 
-class SquareTest(tf.test.TestCase):
+class DataUtilsTest(tf.test.TestCase):
     def test_max_examples(self):
         dataset = load_and_preprocess_dataset('./test', 'test', 4, 4)
+        print len(dataset)
         assert len(dataset) == 4
         dataset = load_and_preprocess_dataset('./test', 'test', 4, 3)
         assert len(dataset) == 3
